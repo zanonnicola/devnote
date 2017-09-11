006f781d57a6ab23f92abe84460b11bb1a3e912a
Nicola Zanon <nicola.zanon86@gmail.com>
2017-09-11 22:18:52 +0100

    Add DraftJS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import {Editor, EditorState} from 'draft-js';
 import logo from './logo.svg';
 import './App.css';
+import './css/editor.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +20,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Editor 
+          editorState={this.state.editorState} 
+          onChange={this.onChange} 
+        />
       </div>
     );
   }
